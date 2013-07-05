dc13d8978b41a5afbb62e112ccc4c47242705dfa
Alberto <alberto.galera@nextlimit.com>
2013-07-05 14:06:07 +0200

    fixed fakewire (again...)

--- src/MainCode.java
+++ src/MainCode.java
@@ -335,9 +335,9 @@ public class MainCode {
             GL11.glBindTexture(GL11.GL_TEXTURE_2D, tmp.get(0));
             
             GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_MAG_FILTER,
-                    GL11.GL_NEAREST);
+                    GL11.GL_LINEAR);
             GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_MIN_FILTER,
-                    GL11.GL_NEAREST);
+                    GL11.GL_LINEAR);
             GL11.glPixelStorei(GL11.GL_UNPACK_ALIGNMENT, 4);
             
             GL11.glTexImage2D(GL11.GL_TEXTURE_2D, 0, GL11.GL_RGBA, decoder.getWidth(), decoder.getHeight(), 0, GL11.GL_RGBA, GL11.GL_UNSIGNED_BYTE, buf);
